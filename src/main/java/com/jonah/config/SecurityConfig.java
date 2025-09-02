e4e17c8a50bae8cb0ee67bf9b8c147fa6cce09d1
Jonah Mutua <mutuajonah38@gmail.com>
2025-09-02 16:10:33 +0300

    Fix: disable CSRF

--- src/main/java/com/jonah/config/SecurityConfig.java
+++ src/main/java/com/jonah/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
         http
-                //.csrf(csrf->csrf.disable)
+                .csrf(csrf->csrf.disable() )
                 .headers(h->h.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))  //Enable in production
                 .sessionManagement(session->session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
